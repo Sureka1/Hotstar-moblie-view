755b3cdaccf34e8b5ae9d242cadccec96b17ef59
Sureka <sureka0107@gmail.com>
2022-03-27 18:12:03 +0530

    first commit

---
+++ Movies.js
@@ -0,0 +1,27 @@
+let movies = [
+    {
+        name: 'DC vs MI 122/4(15.5/20)',
+        des: 'Watch live stream of the TATA IPL 2022 match between Delhi Captails and Mumbai Indians',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/2080/1162080-h-7b35f31019d0'
+    },
+    {
+        name: 'Anupama',
+        des: 'Despite the many challenges thrown her way,Anupama begins a new chapter of her life post-divorce. Head held always high ,whats waiting for her next?',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/8773/1028773-h-48170a73363e'
+    },
+    {
+        name: 'Ghum hai kisikey pyaar mein',
+        des: 'Going beyond the chains of duty, Virat is slowly falling in love with sai.But will pakhi too let go of his broken promises?',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/3006/1073006-h-a2616aa41c91'
+    },
+    {
+        name: 'Raya and the Last Dragon',
+        des: 'Raya, a warrior, sets out to track down Sisu, a dragon, who transferred all her powers into a magical gem which is now scattered all over the kingdom of Kumandra, dividing its people.',
+        image: 'images/slider 4.PNG'
+    },
+    {
+        name: 'Yeh Rishta kya kehlata hai',
+        des: 'Kartik,Naira and sirats daughters are all grown up.Akshara immensely loves Aarohi.But,what happens when they are caught in a love triangle with Abhimanyu?',
+        image: 'https://img1.hotstarext.com/image/upload/f_auto,t_web_m_1x/sources/r1/cms/prod/4515/1074515-h-e3467da993bd'
+    }
+]
